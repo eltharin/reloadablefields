0a577415b04cbc364bcecbf07700303699f740c6
eltharin <eltharin18@hotmail.fr>
2023-11-03 12:16:00 +0100

    new Config

--- src/EltharinReloadableFieldBundle.php
+++ src/EltharinReloadableFieldBundle.php
@@ -2,12 +2,19 @@
 
 namespace Eltharin\ReloadableFieldBundle;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Eltharin\ReloadableFieldBundle\Command\CopyRouteFileCommand;
+use Eltharin\ReloadableFieldBundle\Controller\ReloadFieldController;
+use Eltharin\ReloadableFieldBundle\Type\ReloadableEnityType;
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
-use Symfony\Component\Yaml\Parser;
-use Eltharin\ReloadableFieldBundle\DependencyInjection\Configuration;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+use function Symfony\Component\DependencyInjection\Loader\Configurator\service;
 
 class EltharinReloadableFieldBundle extends AbstractBundle
 {
@@ -29,6 +36,30 @@ class EltharinReloadableFieldBundle extends AbstractBundle
 
 	public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
 	{
-		$container->import(__DIR__.'/../config/services.yaml');
+		$container->services()
+			->set(CopyRouteFileCommand::class)
+			->args(['%kernel.project_dir%',])
+			->tag('console.command')
+		;
+
+		$container->services()
+			->set(ReloadableEnityType::class)
+			->args([
+				service(UrlGeneratorInterface::class),
+				service(ContainerBagInterface::class),
+				service(RequestStack::class),
+			])
+			->tag('form.type')
+		;
+
+		$container->services()
+			->set(ReloadFieldController::class)
+			->args([
+				service(RequestStack::class),
+			])
+			->call('setContainer')
+			->tag('controller.service_arguments')
+			->tag('container.service_subscriber')
+		;
 	}
 }
