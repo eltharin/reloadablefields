29b337af063b27b5c1aebab41130783d3b48940c
eltharin <eltharin18@hotmail.fr>
2024-02-11 18:48:44 +0100

    add route params for autowiring

--- src/Controller/ReloadFieldController.php
+++ src/Controller/ReloadFieldController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
 
 class ReloadFieldController extends AbstractController
 {
@@ -20,17 +21,12 @@ class ReloadFieldController extends AbstractController
 
 		$request = Request::create($requestFrom['url'], $requestFrom['method']);
 
-
 		$route = $this->container->get('router')->matchRequest($request);
+		$route['_route_params'] = $route; //-- on mets aussi les parametres de la route ici pour l'autowiring
 
-		$response = $this->forward($route['_controller'], [
-			'_route' => $route['_route'],
-			'_route_params' => [],
-
-		], $requestFrom['query'] ?: $requestFrom['request']);
+		$response = $this->forward($route['_controller'], $route, $requestFrom['query'] ?: $requestFrom['request']);
 
 		$crawler = new Crawler($response->getContent());
-
 		return new Response($crawler->filter('#' . $fieldName)->eq(0)->outerHtml());
 	}
 }
